f467aa6182cfdfac01cfdb6614a02a97d62670f6
Muhammad Asad Kamran <asad@kamilinx.com>
2019-04-14 19:16:16 +0500

    Secondry routes & route styling

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { MessageService } from '../../messages/message.service';
 
-import { Product } from '../product';
+import { Product, ProductResolved } from '../product';
 import { ProductService } from '../product.service';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-edit.component.html',
@@ -15,25 +15,19 @@ export class ProductEditComponent implements OnInit {
   errorMessage: string;
 
   product: Product;
+  private dataIsValid: { [key: string]: boolean } = {};
 
   constructor(private productService: ProductService,
-              private messageService: MessageService, private route: ActivatedRoute, private router:Router) { }
-  
-ngOnInit(): void{
-    this.route.paramMap.subscribe(
-      params => {
-        const id=+params.get('id');
-        this.getProduct(id);
-      }
-    );    
-}              
+              private messageService: MessageService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
-  getProduct(id: number): void {
-    this.productService.getProduct(id)
-      .subscribe(
-        (product: Product) => this.onProductRetrieved(product),
-        (error: any) => this.errorMessage = <any>error
-      );
+  ngOnInit(): void {
+    this.route.data.subscribe(data => {
+      const resolvedData: ProductResolved = data['resolvedData'];
+      this.errorMessage = resolvedData.error;
+      this.onProductRetrieved(resolvedData.product);
+    });
   }
 
   onProductRetrieved(product: Product): void {
@@ -65,8 +59,17 @@ ngOnInit(): void{
     }
   }
 
+  isValid(path?: string): boolean {
+    this.validate();
+    if (path) {
+      return this.dataIsValid[path];
+    }
+    return (this.dataIsValid &&
+      Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true));
+  }
+
   saveProduct(): void {
-    if (true === true) {
+    if (this.isValid()) {
       if (this.product.id === 0) {
         this.productService.createProduct(this.product)
           .subscribe(
@@ -88,9 +91,32 @@ ngOnInit(): void{
   onSaveComplete(message?: string): void {
     if (message) {
       this.messageService.addMessage(message);
-      this.router.navigate(['/products']);
     }
 
     // Navigate back to the product list
+    this.router.navigate(['/products']);
   }
+
+  validate(): void {
+    // Clear the validation object
+    this.dataIsValid = {};
+
+    // 'info' tab
+    if (this.product.productName &&
+      this.product.productName.length >= 3 &&
+      this.product.productCode) {
+      this.dataIsValid['info'] = true;
+    } else {
+      this.dataIsValid['info'] = false;
+    }
+
+    // 'tags' tab
+    if (this.product.category &&
+      this.product.category.length >= 3) {
+      this.dataIsValid['tags'] = true;
+    } else {
+      this.dataIsValid['tags'] = false;
+    }
+  }
+
 }
